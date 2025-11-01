a54d57f8ca93092a6b3be67520506a5fad721613
coder-zrl <coder-zrl@qq.com>
2025-11-01 19:01:47 +0800

    feat: init water glass bar

---
+++ app/_layout.tsx
@@ -0,0 +1,27 @@
+import {NativeTabs, Badge, Icon, Label, VectorIcon} from 'expo-router/unstable-native-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {FontAwesome, MaterialCommunityIcons, MaterialIcons} from "@expo/vector-icons";
+
+export default function TabLayout() {
+    return (
+        <NativeTabs>
+            <NativeTabs.Trigger name="index">
+                <Label>找搭子</Label>
+                <Icon src={<VectorIcon family={FontAwesome} name="search" />} />
+            </NativeTabs.Trigger>
+            <NativeTabs.Trigger name="square">
+                <Label>广场</Label>
+                <Icon src={<VectorIcon family={MaterialIcons} name="explore" />} />
+            </NativeTabs.Trigger>
+            <NativeTabs.Trigger name="messages">
+                <Label>消息</Label>
+                <Icon src={<VectorIcon family={Ionicons} name="chatbubbles" />} />
+                <Badge>9+</Badge>
+            </NativeTabs.Trigger>
+            <NativeTabs.Trigger name="profile">
+                <Label>我</Label>
+                <Icon src={<VectorIcon family={MaterialCommunityIcons} name="face-man" />} />
+            </NativeTabs.Trigger>
+        </NativeTabs>
+    );
+}
